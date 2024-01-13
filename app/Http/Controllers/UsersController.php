9605ff139d67632b96deeecbd45602fbe3cf42e7
tlow <tlowohphara@gmail.com>
2024-01-13 10:12:22 +0000

    user pages

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;                        // 追加
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    //
+    public function index()                                 // 追加       
+    {                                                       // 追加
+        // ユーザ一覧をidの降順で取得
+        $users = User::orderBy('id', 'desc')->paginate(3); // 追加
+
+        // ユーザ一覧ビューでそれを表示
+        return view('users.index', [                        // 追加
+            'users' => $users,                              // 追加
+        ]);                                                 // 追加
+    }
+    public function show($id)                               // 追加
+    {                                                       // 追加
+        $user = User::findOrFail($id);
+
+        // ユーザ詳細ビューでそれを表示
+        return view('users.show', [
+            'user' => $user,
+        ]);
+        
+    }  
+}
